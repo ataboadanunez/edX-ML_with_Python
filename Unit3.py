de683d70816d5a01e521dcdf929b436352af472d
alvaro.taboada <alvaro.taboada.nunez@gmail.com>
2023-03-23 10:29:43 -0300

    adding code for exercises / homework of Unit3

---
+++ Unit3.py
@@ -0,0 +1,14 @@
+#################################################
+# Exercises and Homework of Unit 3 
+##################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import embed
+
+
+
+
+
+
+
+embed()
